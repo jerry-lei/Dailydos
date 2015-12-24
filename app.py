9cd1aabb53480e4e24f00096a61cc820fb389f10
softdev <jerrylei98@gmail.com>
2015-12-24 12:21:37 -0500

    need to transfer test html file to server

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return "1:24AM"
+    return render_template("test.html")
+
 
 if __name__=="__main__":
     application.run(host='0.0.0.0')
