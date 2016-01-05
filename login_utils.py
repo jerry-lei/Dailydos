8bb129910651b9cb9faa7835d54b2df6cfb3014d
softdev <jerrylei98@gmail.com>
2016-01-05 11:47:37 -0500

    a bit of work in login

--- login_utils.py
+++ login_utils.py
@@ -1,11 +1,31 @@
 import sqlite3
 import re
+#need to switch to mongo for server-sided scripts
+#use string.isalnum() checks for alphanumeric and spaces
 
-def authenticate(username,password):
+def clean(text):
+    return re.sub(r'\W+', '', text)
+
+def if_all_alphanumeric(username, password):
+    if !username.isalnum():
+        return False
+    if !password.isalnum():
+        return False
+    return True
+
+def authenticate(username, password):
+    """
+    Parameters: Username
+
+
+    """
+    if if_all_alphanumeric(username,password):
+        ##
+        ##
+        
+    return False
+
+def create_user(username, password):
     uname = username
-    conn = sqlite3.connect("../login.db")
-    #second connection for file settings
-    c = conn.cursor()
-
-
+    pword = password
 
