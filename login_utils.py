3c902b968b0530b6a31a20d9e2c085f202544fb0
softdev <jerrylei98@gmail.com>
2016-01-06 11:42:53 -0500

    added comments to login functions

--- login_utils.py
+++ login_utils.py
@@ -13,12 +13,25 @@ def if_all_alphanumeric(username, password):
         return False
     return True
 
+def error_code_list():
+    return """
+    Parameter: authenticate, create_user, change password, 
+    If code_list contains: 
+    001 >>  username and password matches, no return
+    002 >>  username and password does not match
+    003 >>  username is not alphanumeric
+    004 >>  password is not alphanumeric
+    005 >>  username is already taken
+    """        
+
+
 def authenticate(username, password):
     """
-    Parameters: Username
-
-
+    Parameters: Username, Password
+    Checks db if username matches password
+    Returns list of codes located in error_code_list, empty list if all conditions met
     """
+    error_codes = [] #add to using error_codes.append(int)
     if if_all_alphanumeric(username,password):
         ##
         ##
@@ -26,6 +39,12 @@ def authenticate(username, password):
     return False
 
 def create_user(username, password):
+    """
+    Parameters: Username, Password
+    Checks db if username exists. Puts username and password into db.
+    Returns list of codes located in error_code_list, empty list if all conditions met
+    """
+    error_codes = [] #add to using error_codes.append(int)
     uname = username
     pword = password
-
+    
