64d797eb34c24d59071e71075b375be9f42cc0da
softdev <jerrylei98@gmail.com>
2016-01-07 11:40:02 -0500

    some changes

--- login_utils.py
+++ login_utils.py
@@ -1,7 +1,10 @@
-import sqlite3
+from pymongo import MongoClient
 import re
 #need to switch to mongo for server-sided scripts
 #use string.isalnum() checks for alphanumeric and spaces
+
+connection = MongoClient()
+database = connection['database']
 
 def clean(text):
     return re.sub(r'\W+', '', text)
@@ -13,30 +16,17 @@ def if_all_alphanumeric(username, password):
         return False
     return True
 
-def error_code_list():
-    return """
-    Parameter: authenticate, create_user, change password, 
-    If code_list contains: 
-    001 >>  username and password matches, no return
-    002 >>  username and password does not match
-    003 >>  username is not alphanumeric
-    004 >>  password is not alphanumeric
-    005 >>  username is already taken
-    """        
-
-
 def authenticate(username, password):
     """
     Parameters: Username, Password
     Checks db if username matches password
     Returns list of codes located in error_code_list, empty list if all conditions met
     """
-    error_codes = [] #add to using error_codes.append(int)
-    if if_all_alphanumeric(username,password):
-        ##
-        ##
-        
-    return False
+    connection = MongoClient()
+    cursor = database.logins.find({'username': username, 'password':password})
+    for r in cursor:
+        return True;
+    return False;
 
 def create_user(username, password):
     """
