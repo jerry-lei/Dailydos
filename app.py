285c6cfdc1bad198e0c1920398ce5cb24baae0f5
softdev <jerrylei98@gmail.com>
2016-01-15 00:44:23 -0500

    fixed what i thought needed to be fixed, still not working "reload: Unknown Instance" error

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 import login_utils
 application = Flask(__name__)
 
+@application.route"/testserver")
+def test():
+    return "hello"
+
 
 @application.route("/", methods=['GET','POST'])
 def home():
@@ -17,38 +21,32 @@ def home():
             if (password == confirm):
                 #username and password lengths check
                 if len(user)<4:
-                    return render_template('home.html',error2="Username must be longer than 4 characters")
+                    return render_template('home.html',errorC="Username must be longer than 4 characters")
                 if len(password)<8:
-                    return render_template('home.html',error2="Password must be longer than 8 characters")
+                    return render_template('home.html',errorC="Password must be longer than 8 characters")
                 #account created successfully
                 if  login_utils.create_user(user,password):
-                    return render_template('home.html',success="Account successfully created! Login to access DailyDos.")
+                    return render_template('home.html',successC="Account successfully created! Login to access DailyDos.")
                 #username taken error
                 else:
-                    return render_template('home.html',error2="Username already in use. Please chose a different username")            
+                    return render_template('home.html',errorC="Username already in use. Please chose a different username")            
             else:
-                return render_template('home.html',error2="Passwords do not match")
+                return render_template('home.html',errorC="Passwords do not match")
         #Login
         #if credentials valid, log them in with session
         if button == "Login":
-            user = request.form['username']
-            password = request.form['password']
+            user = request.form['login_username']
+            password = request.form['login_password']
             if login_utils.authenticate(user,password):
                 if 'n' not in session:
                     session['n'] = uname
                     return redirect(url_for('home'))
                 #else renders login w/ error message
             else:
-                return render_template("home.html",error="Invalid Username or Password")
+                return render_template("home.html",errorL="Invalid Username or Password")
 
-@application.route("/logout")
-def logout():
-    session['user'] = "Anonymous"
-    session['logged_in'] = False
-    return redirect('/')
 
-if __name__=="__main__":
-    
+if __name__=="__main__":    
     application.run(host='0.0.0.0')
     """
     application.debug = True
